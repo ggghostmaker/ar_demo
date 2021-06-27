3160abd07559e45f2587b0740743421291fd9c41
ggghostmaker <85697298+ggghostmaker@users.noreply.github.com>
2021-06-27 22:18:00 +0530

    Add files via upload

---
+++ Assets/ImageTargetEvent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class ImageTargetEvent : MonoBehaviour, ITrackableEventHandler
+{
+    public TrackableBehaviour trackable;
+    public UnityEvent onTrackableFound;
+    public UnityEvent onTrackableLost;
+
+    void OnEnable()
+    {
+        trackable.RegisterTrackableEventHandler(this);
+    }
+
+    void OnDisable()
+    {
+        trackable.UnregisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if(newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            onTrackableFound.Invoke();
+        }
+        else
+        {
+            onTrackableLost.Invoke();
+        }
+    }
+}
